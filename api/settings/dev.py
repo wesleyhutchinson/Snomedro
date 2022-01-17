e1659644025d42aa9ec35a2124fd7882405efcd2
wesleyhutchinson <w.hutchinson@lancaster.ac.uk>
2022-01-17 16:43:54 +0000

    first_app

--- api/settings/dev.py
+++ api/settings/dev.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'api.snomed' # noqa: F403, F401
+
 ]
 
 MIDDLEWARE = [
